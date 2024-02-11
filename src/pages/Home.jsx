f2c952f0c5a60b57bfa1e6a4aefb21b183511f30
NonoLeRobot <sannier.renaud@gmail.com>
2024-02-11 02:49:45 +0100

    herthreth

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+export default function Login() {
+    return (
+    <>
+
+        <h1></h1>
+        <div className="home">
+          <h2 className="desc">
+            Share your files with anyone, for free and without limits.
+          </h2>
+          <h3 className="sub-desc">
+            Upload large files without any size limit per file
+          </h3>
+        </div>
+
+    </>
+  );
+}
+
