1712ec5e18e91e8b00499f2efb0d4906d8bafc0e
NonoLeRobot <sannier.renaud@gmail.com>
2024-02-11 03:12:06 +0100

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Login() {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> COOOOOOOLL
+              React deploy app
             </p>
             <a
               className="App-link"
